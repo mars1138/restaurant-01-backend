2dd78b96692e1ba8f46818bc351ee1c0af340d41
LearnToCode1138 <mdelgado1138@hotmail.com>
2022-07-04 16:24:44 -0700

    update cors

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const app = express();
 app.use(bodyParser.json());
 
 app.use((req, res, next) => {
+  res.setHeader('Content-Type', 'application/json');
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader(
     'Access-Control-Allow-Headers',
