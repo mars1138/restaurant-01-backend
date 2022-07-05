470d522457bfed3c4972723fa3b94e73fe905e08
LearnToCode1138 <mdelgado1138@hotmail.com>
2022-07-04 20:10:39 -0700

    fix listen port

--- app.js
+++ app.js
@@ -65,7 +65,7 @@ mongoose
     { useNewUrlParser: true, useUnifiedTopology: true }
   )
   .then(() => {
-    app.listen(5000);
+    app.listen(process.env.PORT || 5000);
   })
   .catch((err) => {
     console.log();
